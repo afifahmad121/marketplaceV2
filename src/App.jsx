9b021d7431cd0c0dd1f1ee2239f8bd3a36ee9a32
afifahmad121 <afifahmad121@gmail.com>
2026-07-16 21:36:11 +0700

    Set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Footer } from "./components/layout/Footer";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./components/layout/Login";
 import { Registration } from "./components/layout/Registration";
+import ProtectedRoute from "./components/layout/ProtectedRoute";
 
 export default function App() {
   return (
@@ -12,17 +13,40 @@ export default function App() {
       <>
         <Navbar />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Footer />
-              </>
-            }
-          />
+          {/* public Routes */}
+          <Route path="/" element={<Hero />} />
+          <Route path="/" element={<Footer />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Registration" element={<Registration />} />
+
+          {/* Protected Route (harus logi)  */}
+
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute>
+                <Users />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/users/:id"
+            element={
+              <ProtectedRoute>
+                <UserDetail />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/register"
+            element={
+              <ProtectedRoute>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </>
     </main>
