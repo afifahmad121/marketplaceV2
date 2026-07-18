e04e10360b0c37f9033b12861f659c5d8dafae5d
afifahmad121 <afifahmad121@gmail.com>
2026-07-19 01:42:47 +0700

    edit button classname rounded-md

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -5,7 +5,7 @@ export const Button = ({ title, to }) => {
     <Link to={to}>
       <button
         className="bg-button transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:bg-indigo-500 ...
- text-fontWhite text-sm px-6 py-3 right-4 left-4 mr-10 mt-3 mb-3 rounded-full flex items-center"
+ text-fontWhite text-sm py-2 px-4 right-4 left-4 mr-10 mt-3 mb-3 rounded-md flex items-center"
       >
         {title}
       </button>
